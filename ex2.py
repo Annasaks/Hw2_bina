8035805958df68e7a804d25baf0691ab4592a37f
annasaks <anna.sakoun@campus.technion.ac.il>
2025-01-08 11:44:58 +0200

    aa

--- ex2.py
+++ ex2.py
@@ -9,6 +9,8 @@ class GringottsController:
     def __init__(self, map_shape, harry_loc, initial_observations):
         user_map = list([map_shape[0]][map_shape[1]])
         actual_loc = harry_loc
+        neighbors = list(3)
+
         self.get_next_action(self, initial_observations)
 
     def get_next_action(self, observations):
